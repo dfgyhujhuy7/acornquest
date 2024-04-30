6b0755c425963084688f0dcf5ea01cf8a274d691
dfgyhujhuy7 <js1flukeprc@hotmail.com>
2024-04-30 07:32:58 +0700

    aptos

--- index.js
+++ index.js
@@ -1,4 +1,2 @@
-const person = { name: "Alice", age: 30 };
-if (person.hasOwnProperty("age")) {
-  console.log("The person has an age property");
-}
+const countOccurrences = (arr, value) =>
+  arr.reduce((a, v) => (v === value ? a + 1 : a), 0);
